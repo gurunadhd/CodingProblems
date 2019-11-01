46b552a546ab169ee15623cfc855c25e316a9ec6
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2019-11-01 13:42:33 -0700

    Day_3

---
+++ 30daysofcode/30daysofcode/Day_3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30daysofcode
+{
+    class Day_3
+    {
+        /*
+         * Objective
+            In this challenge, we're getting started with conditional statements. Check out the Tutorial tab for learning materials and an instructional video!
+
+            Task
+            Given an integer, , perform the following conditional actions:
+
+            If  is odd, print Weird
+            If  is even and in the inclusive range of  to , print Not Weird
+            If  is even and in the inclusive range of  to , print Weird
+            If  is even and greater than , print Not Weird
+            Complete the stub code provided in your editor to print whether or not  is weird.
+
+            Input Format
+
+            A single line containing a positive integer, .
+
+            Constraints
+
+            Output Format
+
+            Print Weird if the number is weird; otherwise, print Not Weird.
+         */
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (2 <= N && N <= 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (6 <= N && N<= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (N > 20)
+            {
+                Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
