c03c9f0a1a2d796892d4ce935227a9f0e24105f4
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2019-11-01 12:47:29 -0700

    Day_2

---
+++ 30daysofcode/30daysofcode/Day_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30daysofcode
+{
+    class Day_2
+    {
+        // Complete the solve function below.
+        static void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            Console.WriteLine(Convert.ToInt32(meal_cost * (100 + tip_percent + tax_percent) / 100));
+
+        }
+
+        static void Main(string[] args)
+        {
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+            solve(meal_cost, tip_percent, tax_percent);
+        }
+    }
+}
