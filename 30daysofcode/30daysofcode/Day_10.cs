49f61d96fd2bea5a8297a7ee87d179d037222d22
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2019-11-09 00:06:10 -0800

    Day_11

--- 30daysofcode/30daysofcode/Day_10.cs
+++ 30daysofcode/30daysofcode/Day_10.cs
@@ -6,6 +6,23 @@ namespace _30daysofcode
 {
     class Day_10
     {
+        /*
+         Objective
+        Today, we're working with binary numbers. Check out the Tutorial tab for learning materials and an instructional video!
+
+        Task
+        Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation.
+
+        Input Format
+
+        A single integer, .
+
+        Constraints
+
+        Output Format
+
+        Print a single base- integer denoting the maximum number of consecutive 's in the binary representation of .
+         */
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
