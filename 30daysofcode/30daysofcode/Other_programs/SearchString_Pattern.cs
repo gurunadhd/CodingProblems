c3efa7e6180bd66126dcc29a259dc9ddc7b74baa
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2020-08-05 12:10:55 -0700

    Search_string pattern

---
+++ 30daysofcode/30daysofcode/Other_programs/SearchString_Pattern.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace _30daysofcode.Other_programs
+{
+    class SearchString_Pattern
+	{
+		public static bool match_pattern(string s1, string s2)
+        {
+            if (s1 == s2)
+            {
+				return true;
+            }
+            else
+            {
+				return false;
+            }
+		}
+
+
+		static void Main(String[] args)
+		{
+			Console.WriteLine("print string");
+			string T = Console.ReadLine();
+			Console.WriteLine("print pattern string");
+			string pattern = Console.ReadLine();
+
+			List<int> indexes = new List<int>();
+			int i=0;
+			while(i<=T.Length-pattern.Length)
+            {
+				if (match_pattern(T.Substring(i, pattern.Length), pattern))
+				{
+					indexes.Add(i);
+					i = i + pattern.Length;
+					
+				}
+                else
+                {
+					i++;
+                }
+            }
+			Console.WriteLine("indexes of pattern string");
+			foreach (int m in indexes)
+            {
+				Console.WriteLine(m);
+			}
+		}
+	}
+}
