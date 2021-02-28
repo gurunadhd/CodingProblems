51b1105eedc6e0f3ca3e5dcf77e9f2d982a9f193
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2021-02-27 19:31:51 -0800

    Using a read7() method that returns 7 characters from a file, implement readN(n) which reads n characters.
    For example, given a file with the content “Hello world”, three read7() returns “Hello w”, “orld” and then “”.

---
+++ CodingProblems/CodingProblems/DailyCodingProblem/Read_N_Char.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingProblems.DailyCodingProblem
+{
+    class Read_N_Char
+    {
+        /*  Using a read7() method that returns 7 characters from a file, implement readN(n) which reads n characters.
+            For example, given a file with the content “Hello world”, three read7() returns “Hello w”, “orld” and then “”.
+         */
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("privide file path to read");
+            string file_path = Console.ReadLine();
+
+            Console.WriteLine("No. of characters to read");
+            int n = Int32.Parse(Console.ReadLine());
+
+            string content = System.IO.File.ReadAllText(file_path);
+            int current_index = 0;
+            while (current_index<= content.Length)
+            {
+                if(current_index+n <= content.Length)
+                    Console.WriteLine(content.Substring(current_index,n));
+                else
+                    Console.WriteLine(content.Substring(current_index));
+                current_index = current_index + n;
+            }
+        }
+    }
+}
