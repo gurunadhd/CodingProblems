3fc52ce55b939b753a3e49503b737314b95a7577
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2021-03-30 18:59:25 -0700

    Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
    Notice that you may not slant the container.
        Input: height = [1,8,6,2,5,4,8,3,7]
    Output: 49
    Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.

---
+++ CodingProblems/CodingProblems/LeetCode/Container_With_Most_Water.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingProblems
+{
+    /*
+     * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+Notice that you may not slant the container.
+    Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+Notice that you may not slant the container.
+     */
+    class Container_With_Most_Water
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("print array of heights seperated by space");
+            string heights = Console.ReadLine();
+            string[] m = heights.Split(" ");
+            int[] heightArray = new int[m.Length];
+            for (int i = 0; i < m.Length; i++)
+            {
+                heightArray[i] = Int32.Parse(m[i]);
+            }
+
+            Console.WriteLine(MaxArea(heightArray));
+        }
+        public static int MaxArea(int[] height)
+        {
+            int area = 0;
+            int i = 0;
+            int j = height.Length - 1;
+            while (i < j)
+            {
+                if (Math.Min(height[i], height[j]) * Math.Abs(i - j) > area)
+                    area = Math.Min(height[i], height[j]) * Math.Abs(i - j);
+                if (height[i] < height[j])
+                    i++;
+                else
+                    j--;
+
+            }
+            return area;
+
+        }
+    }
+}
