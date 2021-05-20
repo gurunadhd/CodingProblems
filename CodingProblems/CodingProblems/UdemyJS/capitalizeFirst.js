f1e953fd194ae80ee8fcb5fb2b74d69490cf304f
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2021-05-20 15:17:51 -0700

    updated programs to use recursive function instead of recursive helper

--- CodingProblems/CodingProblems/UdemyJS/capitalizeFirst.js
+++ CodingProblems/CodingProblems/UdemyJS/capitalizeFirst.js
@@ -1,18 +1,28 @@
 //given array of strings capitalize first letter of each string
 
-function capitalizeFirst(arr){
+//function capitalizeFirst(arr){
 
-    let returnArr = []
+//     let returnArr = []
 
-    helper(arr)
+//     helper(arr)
 
-    function helper(arr){
+//     function helper(arr){
 
-        if(arr.length==0) return returnArr
-        return returnArr.push(arr[0][0].toUpperCase()+arr[0].substring(1)), helper(arr.splice(1))
-    }
+//         if(arr.length==0) return returnArr
+//         return returnArr.push(arr[0][0].toUpperCase()+arr[0].substring(1)), helper(arr.splice(1))
+//     }
 
-    return returnArr
+//     return returnArr
+// }
+
+function capitalizeFirst(array) {
+    if (array.length === 1)
+      return [array[0][0].toUpperCase() + array[0].substr(1)]
+
+    const res = capitalizeFirst(array.slice(0, -1))
+    const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1)
+    res.push(string)
+    return res
 }
 
 console.log(capitalizeFirst(['car','taco','banana']))
--- CodingProblems/CodingProblems/UdemyJS/capitalizeWords.js
+++ CodingProblems/CodingProblems/UdemyJS/capitalizeWords.js
@@ -1,16 +1,28 @@
 //given array of strings capitalize each string
 
 
+// function capitalizeWords(arr){
+//     let returnArr =[]
+
+//     helper(arr)
+//     function helper(arr){
+//         if(arr.length ==0) return //returnArr
+//         return returnArr.push(arr[0].toUpperCase()),helper(arr.splice(1))
+//     }
+
+//     return returnArr
+// }
+
+
 function capitalizeWords(arr){
-    let returnArr =[]
+    if(arr.length==1) return [arr[0].toUpperCase()] //to retun initial string in array otherArr.push would not work otherwise
 
-    helper(arr)
-    function helper(arr){
-        if(arr.length ==0) return returnArr
-        return returnArr.push(arr[0].toUpperCase()),helper(arr.splice(1))
-    }
-
-    return returnArr
+    const otherArr = capitalizeWords(arr.slice(0, -1))
+    const capitalizedWord = arr[arr.length-1].toUpperCase()
+    otherArr.push(capitalizedWord)
+    
+    return otherArr
 }
-
-console.log(capitalizeWords(['i', 'am', 'learning', 'recursion']))
+console.time('test')
+console.log(capitalizeWords(['i', 'am', 'learning', 'recursion',]))
+console.timeEnd('test')
