d647bebf0b53e0758f022383952b7945eca0b70d
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2021-06-10 14:16:10 -0700

    optized code to inlude noSwaps flag that helps to sort quickly for already nearly sorted arrays

--- CodingProblems/CodingProblems/UdemyJS/bubble.js
+++ CodingProblems/CodingProblems/UdemyJS/bubble.js
@@ -1,17 +1,21 @@
 //bubble sort
-function bubble(arr){
+//optized code to inlude noSwaps flag that helps to sort quickly for already nearly sorted arrays
 
+function bubble(arr){
+    var noSwaps
     for(var i=0;i<arr.length;i++){
-        for(var j=0;j<arr.length-i;j++){
+        noSwaps=true
+        for(var j=0;j<arr.length-1-i;j++){
             if(arr[j]<arr[j+1]){
                 var temp = arr[j]
                 arr[j]=arr[j+1]
                 arr[j+1]=temp
+                noSwaps = false
             }
         }
+        if(noSwaps) break
     }
-
     return arr
 }
 
-console.log(bubble([2,1,3,4,9,5]))
+console.log(bubble([2,1,3,4,9,5,10,11,12,13,8]))
